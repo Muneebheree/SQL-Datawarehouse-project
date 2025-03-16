3effe494fb86d7a371e8e7643a91a8cd6a829b8a
Moneeb Abdullah <161298344+Muneebheree@users.noreply.github.com>
2025-03-16 21:53:57 +0000

    Create storedproc_load_bronze.sql

---
+++ scripts/bronze/storedproc_load_bronze.sql
@@ -0,0 +1,59 @@
+CREATE OR ALTER PROCEDURE Bronze.load_bronze AS
+BEGIN
+
+
+	TRUNCATE TABLE Bronze.crm_cust_info
+	BULK INSERT Bronze.crm_cust_info
+	FROM 'C:\My Data\DE-Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH ( 
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+
+	TRUNCATE TABLE Bronze.crm_prd_info
+	BULK INSERT Bronze.crm_prd_info
+	FROM 'C:\My Data\DE-Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH ( 
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE Bronze.crm_sales_details
+	BULK INSERT Bronze.crm_sales_details
+	FROM 'C:\My Data\DE-Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH ( 
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE Bronze.erp_cust_az12
+	BULK INSERT Bronze.erp_cust_az12
+	FROM 'C:\My Data\DE-Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH ( 
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE Bronze.erp_loc_a101
+	BULK INSERT Bronze.erp_loc_a101
+	FROM 'C:\My Data\DE-Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	WITH ( 
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE Bronze.erp_px_cat_g1v2
+	BULK INSERT Bronze.erp_px_cat_g1v2
+	FROM 'C:\My Data\DE-Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH ( 
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	); 
+END
